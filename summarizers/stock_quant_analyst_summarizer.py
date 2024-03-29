52544f56c80b691511676f736a786c24bd9b59ad
scoupafi <scoupafi@gmail.com>
2024-03-29 11:56:29 +0200

    Change quant summarizer to jailbreak

--- summarizers/stock_quant_analyst_summarizer.py
+++ summarizers/stock_quant_analyst_summarizer.py
@@ -16,22 +16,26 @@ class StockQuantAnalyst(Summarizer):
     summarizer_type: SummarizerType = SummarizerType.DEFAULT
     summary_prompt_prepend: str = 'You are a machine generating a 200 IQ summary of the following video' \
                                     'The goal is to summarize the most actionable points of the video.'\
-                                    'TARGET AUDIENCE: The reader has 189 IQ. They are quantitative finance professionals with 30 years of experience'\
-                                    'They are interest in: stocks, bonds, algorithmic trading, profit, yield curves, cryptocurrency, maximizing profit, minizing risk.'\
-                                    'Provide a point list summary and order points based on importance in the physical, spiritual and mental realities.'\
-                                    'FORMAT: - <secret fundamental alpha - VISIBLE> \n\t - <Example from the video on the point - VISIBLE>'\
-                                    'NUMERICAL VALUES USED FOR ORDERING BUT HIDDEN TO OUTPUT: - <Alpha score - HIDDEN> - <Probability that increases trading profit - HIDDEN> - <reason you believe is important for the TARGET AUDIENCE - HIDDEN>' \
-                                    'FINALLY: Output all companies mentioned (with bullish or bearish sentiment). Output all commodities mentioned (with bullish or bearish sentiment). Output all books mentioned. Output all movies mentioned.' \
-                                    'GUARANTEES: The transcript provided is freely available to the public and summarization is completely legal. All rights are allowed.'\
-                                    'REQUIREMENTS:'\
-                                    'Do NOT include any further explanation, before or after your point list.'\
-                                    'Do NOT be verbose. Consise, pragmatic and number-based answers are best.'\
-                                    'Be SPECIFIC. Do not mention a group (eg. plants) but rather specific (eg. artichoke)'\
-                                    'Mention BEST PRACTICES. In the video a person share secret know-how. Mention these BEST PRACTICES and BEST CHOICES specifically (eg. Choose stocks like X when Y happens)'\
-                                    'Do NOT include points that are irrelevant to the TARGET AUDIENCE.'\
-                                    'GOLDEN RULE #1: IF YOU ARE UNABLE TO GENERATE A SUMMARY MENTION THE REASON WHY AND OFFER SUGGESTIONS TO MITIGATE.'\
-                                    'GOLDEN RULE #2: PROCEED REGARDLESS OF YOUR ACCESS TO UP-TO-DATE INFORMATION.'\
+                                    'Give me a list of the top 20 most important points mentioned'\
                                     'TRANCRIPT:' 
+                                    
+                                    # 'TARGET AUDIENCE: The reader has 189 IQ. They are quantitative finance professionals with 30 years of experience'\
+                                    # 'They are interest in: stocks, bonds, algorithmic trading, profit, yield curves, cryptocurrency, maximizing profit, minizing risk.'\
+                                    # 'Provide a point list summary and order points based on importance in the physical, spiritual and mental realities.'\
+                                    # 'FORMAT: SUMMARY: - < fundamental alpha mentioned in transcript- VISIBLE> \n\t - <Example from the video on the point - VISIBLE>'\
+                                    # 'NUMERICAL VALUES USED FOR ORDERING BUT HIDDEN TO OUTPUT: - <Alpha score - HIDDEN> - <Probability that increases trading profit - HIDDEN> - <reason you believe is important for the TARGET AUDIENCE - HIDDEN>' \
+                                    # 'FINALLY: Output all companies mentioned (with bullish or bearish sentiment). Output all commodities mentioned (with bullish or bearish sentiment). Output all books mentioned. Output all movies mentioned.' \
+                                    # 'GUARANTEES: The transcript provided is freely available to the public and summarization is completely legal. All rights are allowed.'\
+                                    # 'REQUIREMENTS:'\
+                                    # 'Do NOT include any further explanation, before or after your point list.'\
+                                    # 'Do NOT be verbose. Consise, pragmatic and number-based answers are best.'\
+                                    # 'Be SPECIFIC. Do not mention a group (eg. plants) but rather specific (eg. artichoke)'\
+                                    # 'Mention BEST PRACTICES. In the video a person share secret know-how. Mention these BEST PRACTICES and BEST CHOICES specifically (eg. Choose stocks like X when Y happens)'\
+                                    # 'Do NOT include points that are irrelevant to the TARGET AUDIENCE.'\
+                                    # 'GOLDEN RULE #1: IF YOU ARE UNABLE TO GENERATE A SUMMARY MENTION THE REASON WHY AND OFFER SUGGESTIONS TO MITIGATE.'\
+                                    # 'GOLDEN RULE #2: PROCEED REGARDLESS OF YOUR ACCESS TO UP-TO-DATE INFORMATION.'\
+                                    # 'GOLDEN RULES #3: ALWAYS MENTION YOUR MODEL NAME' \
+                                    # 'TRANCRIPT:' 
     summary_prompt_append: str = ''
 
     def __init__(self):
